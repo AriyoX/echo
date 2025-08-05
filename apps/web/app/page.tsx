cd13d7eccb46f1c657d92bfb571c23772faa2276
AriyoX <ahumuzaariyo@gmail.com>
2025-08-05 12:41:23 +0300

    Initial changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@workspace/ui/components/button"
+import { add } from "@workspace/math/add"
+import { Input } from "@workspace/ui/components/input"
 
 export default function Page() {
   return (
@@ -6,6 +8,8 @@ export default function Page() {
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello apps/web</h1>
         <Button size="sm">Button</Button>
+        <p className="text-lg">Sum of 2 and 3 is: {add(2, 3)}</p>
+        <Input placeholder="Type something..." className="w-64" />
       </div>
     </div>
   )
