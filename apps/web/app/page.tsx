58740ab89ea6ba4fecdf7d0ef2c7ad36c6cf40bb
AriyoX <ahumuzaariyo@gmail.com>
2025-08-06 21:22:49 +0300

    Added convex backend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,21 @@
+"use client"
+
+import { useMutation, useQuery } from "convex/react"
+import { api } from "@workspace/backend/_generated/api"
 import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input"
 
 export default function Page() {
+  const users = useQuery(api.users.getMany)
+  const addUser = useMutation(api.users.add)
+
   return (
     <div className="flex items-center justify-center min-h-svh">
+      <p>apps/web</p>
+      <Button onClick={() => addUser()}>
+        Add User
+      </Button>
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
-        <Button size="sm">Button</Button>
-        <p className="text-lg">Sum of 2 and 3 is: {add(2, 3)}</p>
-        <Input placeholder="Type something..." className="w-64" />
+        {JSON.stringify(users, null, 2)}
       </div>
     </div>
   )
